ba0fe785951dcd0f42b8aefec1ec5574db4dad9c
허민욱 <bill.heo@tosslab.com>
2015-04-16 18:52:13 +0900

    invitation 추가

---
+++ app/src/main/java/com/example/hmw/invitation/InvitationActivity.java
@@ -0,0 +1,135 @@
+package com.example.hmw.invitation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
+import com.example.hmw.lambdatest.R;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by Bill Minwook Heo on 15. 4. 16..
+ */
+@EActivity(R.layout.activity_invitation)
+public class InvitationActivity extends Activity {
+
+
+    @Click(R.id.kakaoButton)
+    public void clickkakao() {
+        //com.kakao.talk
+        //jp.naver.line.android
+        //com.tencent.mm
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setPackage("com.kakao.talk");
+        intent.putExtra(Intent.EXTRA_TEXT, "AAA");
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+
+    @Click(R.id.LineButton)
+    public void clickLine() {
+        //com.kakao.talk
+        //jp.naver.line.android
+        //com.tencent.mm
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setPackage("jp.naver.line.android");
+        intent.putExtra(Intent.EXTRA_TEXT, "AAA");
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+
+    @Click(R.id.WeChatButton)
+    public void clickWeChat() {
+        //com.kakao.talk
+        //jp.naver.line.android
+        //com.tencent.mm
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setPackage("com.tencent.mm");
+        intent.putExtra(Intent.EXTRA_TEXT, "AAA");
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+
+    @Click(R.id.phoneButton)
+    public void clickPhone() {
+        //com.kakao.talk
+        //jp.naver.line.android
+        //com.tencent.mm
+
+        /*Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        //intent.setType(ContactsContract.CommonDataKinds.Email.CONTENT_TYPE);
+        intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(intent.createChooser(intent, "select email"), 0);*/
+
+    }
+
+    @Click(R.id.messengerButton)
+    public void clickMessenger() {
+        //com.facebook.orca
+        //808900692521335
+
+        String EXTRA_PROTOCOL_VERSION = "com.facebook.orca.extra.PROTOCOL_VERSION";
+        String EXTRA_APP_ID = "com.facebook.orca.extra.APPLICATION_ID";
+        int PROTOCOL_VERSION = 20150416;
+        String YOUR_APP_ID = "808900692521335";
+        int SHARE_TO_MESSENGER_REQUEST_CODE = 1;
+
+        String mimeType = "text/plain";
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setPackage("com.facebook.orca");
+        intent.setType(mimeType);
+        //intent.putExtra(Intent.EXTRA_STREAM, contentUri);
+        intent.putExtra(Intent.EXTRA_TEXT, "AAA");
+        intent.putExtra(EXTRA_PROTOCOL_VERSION, PROTOCOL_VERSION);
+        intent.putExtra(EXTRA_APP_ID, YOUR_APP_ID);
+
+        startActivityForResult(intent, SHARE_TO_MESSENGER_REQUEST_CODE);
+
+
+        /*Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setPackage("com.facebook.orca");
+        intent.putExtra(Intent.EXTRA_TEXT, "AAA");
+        intent.setType("text/plain");
+        startActivity(intent);*/
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("result : ", Integer.toString(resultCode));
+
+        //Cursor cursor = managedQuery(data.getData(), null, null, null, null);
+        /*Cursor cursor = getContentResolver().query(data.getData(), null, null, null, null);
+        String email = null;
+        if (cursor.moveToFirst()) {
+
+            Log.i("getColumnCount : ", Integer.toString(cursor.getColumnCount()));
+            Log.i("getCount : ", Integer.toString(cursor.getCount()));
+            email = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS));
+            Log.i("email : ", email);
+
+            while (cursor.moveToNext()) {
+                Log.i("moveToNext : ", "moveToNext");
+                email = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS));
+                Log.i("email : ", email);
+            }
+
+        }
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        String[] mailaddr = {email};
+
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, mailaddr);
+
+        startActivity(emailIntent);*/
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/example/hmw/lambdatest/TestFragment.java
@@ -0,0 +1,20 @@
+package com.example.hmw.lambdatest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bill Minwook Heo on 15. 4. 10..
+ */
+public class TestFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
